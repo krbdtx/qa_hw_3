c5ff74fc3f63b5c0dd4d4da68c7e23c0bbc5a173
Evgenii <mail@mail.mail>
2024-03-28 21:16:01 +0300

    HW-3_v.3 Error -OK

--- test_suite_search_3.py
+++ test_suite_search_3.py
@@ -2,7 +2,9 @@ from selene import browser, be, have
 import pytest
 
 search_txt = 'yashaka/selene'
+search_txt_error = 'фывфывфывфыввайцйц'
 search_txt_in_brow = 'User-oriented Web UI browser tests in Python. Contribute to '
+search_txt_in_error = 'По введённому запросу ничего не найдено'
 
 def test_01(setting_browser):
     ''''
@@ -14,7 +16,7 @@ def test_01(setting_browser):
     browser.element('[name="q"]').should(be.blank).type(search_txt).press_enter()
     browser.element('[id="search"]').should(have.text(search_txt_in_brow))
 
-@pytest.mark.xfail()
+
 def test_02():
     ''''
         Проверка поиска строки Рамблером
@@ -22,8 +24,8 @@ def test_02():
     '''
 
     browser.open('https://rambler.ru')
-    browser.element('[placeholder="Поиск по интернету"]').should(be.blank).type(search_txt).press_enter()
-    browser.element('[class="serp-item serp-item_card "]').should(have.text(search_txt_in_brow))
+    browser.element('[placeholder="Поиск по интернету"]').should(be.blank).type(search_txt_error).press_enter()
+    browser.element('[class="Error__title--3qsmL"]').should(have.text(search_txt_in_error))
 
 
 def test_03(setting_browser):
